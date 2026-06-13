5ae92dddb389f253d6a33197fce4e997c3e65dd9
mzashah <mz.ashah@outlook.com>
2026-06-13 19:54:10 +0500

    Initial commit: add lib/utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { type ClassValue, clsx } from 'clsx'
+
+export function cn(...inputs: ClassValue[]) {
+  return clsx(inputs)
+}
+
+export function formatCurrency(amount: number, currency = 'USD') {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+  }).format(amount / 100)
+}
+
+export function formatDate(date: Date) {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }).format(date)
+}
